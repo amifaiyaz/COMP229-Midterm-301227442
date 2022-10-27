9cb51b9ed3d072249fd57ad9a98893a2c8c6220a
amifaiyaz <amifaiyaz25@gmail.com>
2022-10-27 19:00:05 -0400

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,3 @@
+//Name: Faiyaz Muhammmad
+export const MongoURI = "mongodb+srv://Faiyaz:faiyaz2507@midterm-comp229-3012274.wto9d92.mongodb.net/?retryWrites=true&w=majority";
+export const Secret = "Static";
